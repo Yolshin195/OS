a4c13125b7c37cf52fa311ae95e767043504225a
Alexey Yolshin <elshin195@gmail.com>
2018-01-03 01:03:08 +0500

    Update sway.sh

--- sway.sh
+++ sway.sh
@@ -17,6 +17,8 @@ cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local -DSOURCE_WLPR
 make
 sudo make install
 
+cd ../..
+
 apt-get install libpcre3 libpcre3-dev libcairo2 libcairo2-dev libpango1.0-0 libpango1.0-dev asciidoc libjson-c3 libjson-c-dev libcap-dev xsltproc libpam0g-dev
 git clone https://github.com/SirCmpwn/sway.git
 cd sway/
